5fdff94dd01f8bd9254b2c0264b5ddc48b076d5c
Novah974-8 <151816697+Novah974-8@users.noreply.github.com>
2026-04-08 17:03:22 +0200

    Version 3 : Ajout de la boucle non bloquante
    Updated host constants and modified socket connection functions to use C_HOST. Refactored receive_data function to use non-blocking select.

--- src/network/comm_py_c.py
+++ src/network/comm_py_c.py
@@ -1,20 +1,22 @@
 import socket
+import select
 import json_utils as j
 
-HOST = "127.0.0.1"
+C_HOST = "127.0.0.1"
 C_PORT = 1040
+
 PY_PORT = 1030
 
 def connect_sock_send():
     python_c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    python_c_socket.connect((HOST, C_PORT))
-    print(f"Connected at {HOST}:{C_PORT}")
+    python_c_socket.connect((C_HOST, C_PORT))
+    print(f"Connected at {C_HOST}:{C_PORT}")
     return python_c_socket
 
 def connect_sock_recv():
     python_c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    python_c_socket.bind((HOST, PY_PORT))
-    print(f"Listen at {HOST}:{PY_PORT}")
+    python_c_socket.bind((C_HOST, PY_PORT))
+    print(f"Listen at {C_HOST}:{PY_PORT}")
     return python_c_socket
 
 def send_data(data, sock):
@@ -22,20 +24,30 @@ def send_data(data, sock):
     sock.close()
 
 def receive_data(sock):
-    response = sock.recv(1024)
-    print("Réponse du serveur :", response.decode())
-    return response.decode()
-    sock.close()
+    ready, _, _ = select.select([sock], [], [], 0)  # 0 = non bloquant
 
-sock = connect_sock_send()
+    if ready:
+        response = sock.recv(1024)
+        msg = response.decode()
+        print("Réponse du serveur :", msg)
+        return msg
+    
+    return None
 
-data = j.create_json(0, 100, 10, 10)
-send_data(data, sock)
 
+# sock = connect_sock_send()
+
+# data = j.create_json(0, 100, 10, 10)
+# send_data(data, sock)
 
 sock = connect_sock_recv()
-msg = receive_data(sock)
-data = j.load_json(msg)
-print(data)
-print(data["uid"])
+
+while True:
+    msg = receive_data(sock)
+
+    if msg:
+        data = j.load_json(msg)
+        print(data)
+        print(data["uid"])
+
 
