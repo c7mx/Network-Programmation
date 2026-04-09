b4b76f81784b5745b4eb3eff3c8086f1258726b8
c7mx <maxime.catteau@insa-cvl.fr>
2026-04-09 07:04:20 +0000

    Refactor Main.py by removing unused code
    Removed unused imports and command options for loading and plotting scenarios.

--- src/Main.py
+++ src/Main.py
@@ -3,11 +3,8 @@ from util.ScenarioMaker import ScenarioMaker
 from model.Battlefield import Battlefield
 from model.Battle import Battle
 from model.BattleMulti import BattleMulti
-from scenario.Lanchester import Lanchester
 from view.GUI import GUI
 from view.Console import Console
-from scenario.ScenarioDataPlotter import PlotLanchester
-from battle_tournament.TournamentManager import run_tournament
 from util.Functions import parse_units_list, parse_range, get_scenario, create_parser, generate_heightmap
 from util.SaveManager import SaveManager
 
@@ -70,35 +67,3 @@ if __name__ == '__main__':
             battle.collectStats = True
 
         battle.start()
-
-
-    if args.command == 'load':
-        view_factory = (lambda bf: GUI(bf)) if args.gui else None
-        battle = SaveManager.load_battle(args.savefile, view_factory=view_factory)
-        battle.start()
-
-    if args.command == 'tourney':
-        print(f"Running tournament with AIs: {args.G}, scenarios: {args.scenarios}, rounds: {args.rounds}")
-        run_tournament(args) # Call run_tournament function
-
-    if args.command == 'plot':
-
-        units_type = parse_units_list(args.units_type)
-
-        ranges = parse_range(args.ranges)
-
-        print("Automated scenario start with parameters:\n")
-        print(f"AI: {args.AI} plotter={args.plotter} scenario={args.scenario}\n")
-        print(f"units_type={units_type} range={ranges} rounds={args.rounds}\n")
-        print("Running..........................................................")
-
-
-        data = {}
-        for unit_type in units_type:
-            for n in ranges:
-                scenario = eval(args.scenario)
-                data[unit_type, n] = scenario(unit_type, n, args.AI).run()
-
-        plotter = eval(args.plotter)
-        plotter(data)
-
