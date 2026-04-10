1d05d158b420473b7840ff913b74dfeff902f8a7
Novah974-8 <151816697+Novah974-8@users.noreply.github.com>
2026-04-10 16:13:56 +0200

    Remove print statements from socket functions
    Removed print statements for connection status and server response.

--- src/network/comm_py_c.py
+++ src/network/comm_py_c.py
@@ -10,13 +10,11 @@ PY_PORT = 1030
 def connect_sock_send():
     python_c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     python_c_socket.connect((C_HOST, C_PORT))
-    print(f"Connected at {C_HOST}:{C_PORT}")
     return python_c_socket
 
 def connect_sock_recv():
     python_c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     python_c_socket.bind((C_HOST, PY_PORT))
-    print(f"Listen at {C_HOST}:{PY_PORT}")
     return python_c_socket
 
 def send_data(sock, uid, hp, x, y, type=None):
@@ -29,7 +27,6 @@ def receive_data(sock):
     if ready:
         response = sock.recv(1024)
         msg = response.decode()
-        print("Réponse du serveur :", msg)
         return msg
     
     return None
