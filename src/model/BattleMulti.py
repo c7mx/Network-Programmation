5e47ad21f3c27bcd17b53c2ee55d6d648ac4500c
majda52 <lhsmajda07@gmail.com>
2026-04-10 10:12:38 +0200

    intégration du mise à jours

--- src/model/BattleMulti.py
+++ src/model/BattleMulti.py
@@ -10,6 +10,20 @@ import webbrowser
 from view.Console import Console
 from view.GUI import GUI
 import time
+from Update import update 
+data_list = [
+    '{"uid": 1999, "hp": 32, "x": 40, "y": 50, "type": "P"}',
+    '{"uid": 1324, "hp": 78, "x": 12, "y": 34, "type": "K"}',
+    '{"uid": 1789, "hp": 45, "x": 22, "y": 5, "type": "C"}',
+    '{"uid": 1543, "hp": 91, "x": 47, "y": 12, "type": "P"}',
+    '{"uid": 1198, "hp": 23, "x": 8, "y": 33, "type": "K"}',
+    '{"uid": 1650, "hp": 67, "x": 49, "y": 0, "type": "C"}',
+    '{"uid": 1432, "hp": 10, "x": 25, "y": 44, "type": "P"}',
+    '{"uid": 1876, "hp": 88, "x": 15, "y": 27, "type": "K"}',
+    '{"uid": 1211, "hp": 52, "x": 3, "y": 19, "type": "C"}',
+    '{"uid": 1999, "hp": 36, "x": 40, "y": 50, "type": "P"}',
+    '{"uid": 1107, "hp": 99, "x": 50, "y": 1, "type": "K"}'
+]
 
 
 class BattleMulti:
@@ -72,6 +86,7 @@ class BattleMulti:
             if not self.paused and self.winner is None:
 
                 for _ in range(self.speed):
+                    update(data_list, battlefield)
                     self.general.play(self.battlefield)
 
                     if self.logger:
