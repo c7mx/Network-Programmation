31cf99a9a2adb5cf3ac35a93ae1442dfeac00d07
Novah974-8 <151816697+Novah974-8@users.noreply.github.com>
2026-04-10 15:06:15 +0200

    Version 1 Checked

--- src/model/Unit.py
+++ src/model/Unit.py
@@ -6,7 +6,7 @@ import network.comm_py_c as NetPy
 import random
 from Constant import STATS_BONUS_FILEPATH, EPSILON, K_ELEVATION_H, K_ELEVATION_D
 from util.CSVLoader import CSVLoader
-import time
+
 
 BONUS_DAMAGE_MATRIX = CSVLoader().load_bonus_damage_matrix(STATS_BONUS_FILEPATH)
 BONUS_ARMOR_MATRIX = CSVLoader().load_bonus_armor_matrix(STATS_BONUS_FILEPATH)
@@ -37,33 +37,6 @@ class Unit(ABC):
     target_pos: tuple[float, float] = None
     
     last_attacker: "Unit" = None    # Unit that last attacked this unit (for Braindead strategy)
-    direction: str = "down"
-    direction_cooldown:float = 0
-
-    def update_direction(self, new_position):
-        old_row, old_col = self.position
-        new_row, new_col = new_position
-
-        dx = new_col - old_col
-        dy = new_row - old_row
-        if self.direction_cooldown < 0:
-            if abs(dx) > abs(dy):
-                if dx > 0.014:
-                    self.direction = "right"
-                    self.direction_cooldown += 1
-                elif dx < -0.014:
-                    self.direction = "left"
-                    self.direction_cooldown += 1
-            else:
-                if dy > 0.014:
-                    self.direction = "down"
-                    self.direction_cooldown += 1
-                elif dy < -0.014:
-                    self.direction = "up"
-                    self.direction_cooldown += 1
-        else : 
-            self.direction_cooldown -= 0.1
-    
 
     # ------------------------------------------------------------------
     # CORE STATUS & UTILITY
@@ -84,8 +57,8 @@ class Unit(ABC):
             self.target_pos = None
 
         # Network Part
-        #sock = NetPy.connect_sock_send()
-        #NetPy.send_data(sock, self.id, self.hp, self.position[0], self.position[1], type=None)
+        sock = NetPy.connect_sock_send()
+        NetPy.send_data(sock, self.id, self.hp, self.position[0], self.position[1], self.name[0])
 
     def set_order(self, order_type: str, target=None, target_pos=None):
         """Assign a new 'move' or 'attack' command with associated targets."""
@@ -193,10 +166,6 @@ class Unit(ABC):
             y - perp_y * side_step
         )
 
-        # Network Part
-        #sock = NetPy.connect_sock_send()
-        #NetPy.send_data(sock, self.id, self.hp, self.position[0], self.position[1], type=None)
-
         if self._try_move(right_pos):
             return
 
@@ -218,8 +187,13 @@ class Unit(ABC):
                 self.set_order("attack", target=blocker)
             return False # We stop no matter what (ally or enemy)
             
-        self.update_direction(new_pos)
         self.position = new_pos
+
+        # Network Part
+        sock = NetPy.connect_sock_send()
+        NetPy.send_data(sock, self.id, self.hp, self.position[0], self.position[1], self.name[0])
+        
+
         return True
 
     def is_enemy(self, other: "Unit") -> bool:
@@ -287,4 +261,3 @@ class Unit(ABC):
     #def __repr__(self):
     #    """Return a readable summary of the unit for debugging."""
     #    return f"{self.name} {self.id} ({self.hp}hp @ {self.position})"
-
