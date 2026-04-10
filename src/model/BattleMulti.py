45c83d2a190a2158e5d944a1232b6d87eec18627
Novah974-8 <151816697+Novah974-8@users.noreply.github.com>
2026-04-10 15:05:07 +0200

    Version 1 Valided
    Refactor BattleMulti to receive data from network and update battlefield.

--- src/model/BattleMulti.py
+++ src/model/BattleMulti.py
@@ -10,21 +10,15 @@ import webbrowser
 from view.Console import Console
 from view.GUI import GUI
 import time
-from Update import update 
-data_list = [
-    '{"uid": 1999, "hp": 32, "x": 40, "y": 50, "type": "P"}',
-    '{"uid": 1324, "hp": 78, "x": 12, "y": 34, "type": "K"}',
-    '{"uid": 1789, "hp": 45, "x": 22, "y": 5, "type": "C"}',
-    '{"uid": 1543, "hp": 91, "x": 47, "y": 12, "type": "P"}',
-    '{"uid": 1198, "hp": 23, "x": 8, "y": 33, "type": "K"}',
-    '{"uid": 1650, "hp": 67, "x": 49, "y": 0, "type": "C"}',
-    '{"uid": 1432, "hp": 10, "x": 25, "y": 44, "type": "P"}',
-    '{"uid": 1876, "hp": 88, "x": 15, "y": 27, "type": "K"}',
-    '{"uid": 1211, "hp": 52, "x": 3, "y": 19, "type": "C"}',
-    '{"uid": 1999, "hp": 36, "x": 40, "y": 50, "type": "P"}',
-    '{"uid": 1107, "hp": 99, "x": 50, "y": 1, "type": "K"}'
-]
+from Update import update
 
+import network.comm_py_c as NetPy
+import network.json_utils as j
+import subprocess
+
+data_list = []
+
+proc = subprocess.Popen(["./network/comm_c_c"])
 
 class BattleMulti:
 
@@ -62,6 +56,8 @@ class BattleMulti:
         running = True
         clock = pygame.time.Clock()
 
+        sock = NetPy.connect_sock_recv()
+
         while running:
 
             # ------------------ Timing ------------------
@@ -85,8 +81,20 @@ class BattleMulti:
             # ------------------ Simulation ------------------
             if not self.paused and self.winner is None:
 
+                
+                msg = NetPy.receive_data(sock)
+
+                if msg:
+                    # print(msg)
+                    data = j.load_json(msg)
+                    data_list.append(data)
+                    # print(data_list)
+                
+
+                update(data_list, self.battlefield)
+
                 for _ in range(self.speed):
-                    update(data_list, self.battlefield)
+
                     self.general.play(self.battlefield)
 
                     if self.logger:
@@ -145,6 +153,7 @@ class BattleMulti:
 
                 # ================= QUIT =================
                 elif event.key == pygame.K_o:
+                    proc.terminate()
                     pygame.quit()
                     exit()
 
