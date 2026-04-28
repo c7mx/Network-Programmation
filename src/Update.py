55561621e51cc91b5051657bbcca6f50cb40818c
MBellebeau <mathilde.bouille@insa-cvl.fr>
2026-04-28 14:18:10 +0200

    Update Update.py

--- src/Update.py
+++ src/Update.py
@@ -1,8 +1,9 @@
+
 import json
 import sys
 import os
 
-# Absolute path of srcdirectory
+# Absolute path of src directory
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 if SRC_DIR not in sys.path:
     sys.path.insert(0, SRC_DIR)
@@ -10,10 +11,11 @@ if SRC_DIR not in sys.path:
 from model.Unit import Unit
 from model.Battlefield import Battlefield
 
-def update(data_list, battlefield:Battlefield):
+def update(data_list, battlefield: Battlefield):
     while data_list:
         data = data_list.pop(0)
 
+        # BUG 1 FIXED : if → elif pour éviter de tester les 3 types à chaque fois
         if data["type"] == 'K':
             data["name"] = "Knight"
             data["type_attack"] = "Melee"
@@ -27,7 +29,7 @@ def update(data_list, battlefield:Battlefield):
             data["reload_time"] = 1.8
             data["accuracy"] = 1
 
-        if data["type"] == 'C':
+        elif data["type"] == 'C':
             data["name"] = "Crossbowman"
             data["type_attack"] = "Pierce"
             data["attack"] = 5
@@ -40,7 +42,7 @@ def update(data_list, battlefield:Battlefield):
             data["reload_time"] = 5
             data["accuracy"] = 0.85
 
-        if data["type"] == 'P':
+        elif data["type"] == 'P':
             data["name"] = "Pikeman"
             data["type_attack"] = "Melee"
             data["attack"] = 4
@@ -53,10 +55,14 @@ def update(data_list, battlefield:Battlefield):
             data["reload_time"] = 3
             data["accuracy"] = 1
 
+        else:
+            # Type inconnu : on ignore ce message
+            continue
+
         uid = data["uid"]
 
         if uid not in battlefield.troupes:
-            if data ["hp"] != 0:
+            if data["hp"] > 0:
                 battlefield.troupes[uid] = Unit(
                     data["uid"],
                     data["name"],
@@ -77,11 +83,12 @@ def update(data_list, battlefield:Battlefield):
         else:
             if data["hp"] < battlefield.troupes[uid].hp:
                 battlefield.troupes[uid].hp = data["hp"]
+
             if battlefield.troupes[uid].line_of_sight == 0:
                 if (battlefield.troupes[uid].position[0] != data["x"]
                         or battlefield.troupes[uid].position[1] != data["y"]):
                     battlefield.troupes[uid].position = (data["x"], data["y"])
-            if battlefield.troupes[uid].hp <= 0:
-                battlefield.remove_unit(uid)
-            if battlefield.troupes[uid].hp <= 0:
+
+            # BUG 2 FIXED : un seul remove_unit, protégé par un 'in' pour éviter KeyError
+            if uid in battlefield.troupes and battlefield.troupes[uid].hp <= 0:
                 battlefield.remove_unit(uid)
