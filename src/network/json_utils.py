8c2215b66dcf558578ff6f8ee4466d74303511fb
MineDX10 <119359999+MineDX10@users.noreply.github.com>
2026-04-29 09:31:57 +0200

    Add create_jsonbisbis

--- src/network/json_utils.py
+++ src/network/json_utils.py
@@ -25,6 +25,20 @@ def create_jsonbis(Req, uid, etat, Post_local):
 
     return msg
 
+
+
+def create_jsonbisbis(Ask, uid, etat, Post_local):
+    data = {
+        "Ask": Ask,
+        "uid": uid,
+        "etat": etat,
+        "Post_local": Post_local
+    }
+
+    msg = json.dumps(data)  # dict → string JSON
+
+    return msg
+    
 def load_json(msg):
 
     data = json.loads(msg)  # string → dict
