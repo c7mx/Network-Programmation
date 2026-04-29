00299e0e67da483c456b1351ddcb6fa816bce1f2
lucasd1er <124613482+lucasd1er@users.noreply.github.com>
2026-04-29 09:48:45 +0200

    Ajout mise à jour propriété vr

--- src/Update.py
+++ src/Update.py
@@ -34,74 +34,82 @@ def is_action_possible(data: dict, battlefield: Battlefield) -> bool:
 
 
 def update(data_list, battlefield: Battlefield):
-    while data_list:
-        data = data_list.pop(0)
-        if not is_action_possible(data, battlefield):
-            continue
-        if data["type"] == 'K':
-            data["name"] = "Knight"
-            data["type_attack"] = "Melee"
-            data["attack"] = 8
-            data["armor"] = 2
-            data["pierce_armor"] = 2
-            data["range"] = 0.01
-            data["line_of_sight"] = 0
-            data["speed"] = 1.35
-            data["attack_delay"] = 0
-            data["reload_time"] = 1.8
-            data["accuracy"] = 1
-        if data["type"] == 'C':
-            data["name"] = "Crossbowman"
-            data["type_attack"] = "Pierce"
-            data["attack"] = 5
-            data["armor"] = 0
-            data["pierce_armor"] = 0
-            data["range"] = 5
-            data["line_of_sight"] = 0
-            data["speed"] = 0.96
-            data["attack_delay"] = 0
-            data["reload_time"] = 5
-            data["accuracy"] = 0.85
-        if data["type"] == 'P':
-            data["name"] = "Pikeman"
-            data["type_attack"] = "Melee"
-            data["attack"] = 4
-            data["armor"] = 0
-            data["pierce_armor"] = 0
-            data["range"] = 0.01
-            data["line_of_sight"] = 0
-            data["speed"] = 1
-            data["attack_delay"] = 0
-            data["reload_time"] = 3
-            data["accuracy"] = 1
-        data["property"] = False
-        uid = data["uid"]
-        if uid not in battlefield.troupes:
-            if data ["hp"] != 0:
-                battlefield.troupes[uid] = Unit(
-                    data["uid"],
-                    data["name"],
-                    data["type"],
-                    data["hp"],
-                    data["type_attack"],
-                    data["attack"],
-                    data["armor"],
-                    data["pierce_armor"],
-                    data["range"],
-                    data["line_of_sight"],
-                    data["speed"],
-                    data["attack_delay"],
-                    data["reload_time"],
-                    data["accuracy"],
-                    (data["x"], data["y"]),
-                    data["property"]
-                )
-        else:
-            if data["hp"] < battlefield.troupes[uid].hp:
-                battlefield.troupes[uid].hp = data["hp"]
-            if battlefield.troupes[uid].line_of_sight == 0:
-                if (battlefield.troupes[uid].position[0] != data["x"]
-                        or battlefield.troupes[uid].position[1] != data["y"]):
-                    battlefield.troupes[uid].position = (data["x"], data["y"])
-            if battlefield.troupes[uid].hp <= 0:
-                battlefield.remove_unit(uid)
+    if "Req" in data:
+        if(data["property"]):
+            data["property"]= False
+
+    elif "Ask" in data and uid // 1000 == Post_Local:
+        data["property"]= True
+    
+    else: 
+        while data_list:
+            data = data_list.pop(0)
+            if not is_action_possible(data, battlefield):
+                continue
+            if data["type"] == 'K':
+                data["name"] = "Knight"
+                data["type_attack"] = "Melee"
+                data["attack"] = 8
+                data["armor"] = 2
+                data["pierce_armor"] = 2
+                data["range"] = 0.01
+                data["line_of_sight"] = 0
+                data["speed"] = 1.35
+                data["attack_delay"] = 0
+                data["reload_time"] = 1.8
+                data["accuracy"] = 1
+            if data["type"] == 'C':
+                data["name"] = "Crossbowman"
+                data["type_attack"] = "Pierce"
+                data["attack"] = 5
+                data["armor"] = 0
+                data["pierce_armor"] = 0
+                data["range"] = 5
+                data["line_of_sight"] = 0
+                data["speed"] = 0.96
+                data["attack_delay"] = 0
+                data["reload_time"] = 5
+                data["accuracy"] = 0.85
+            if data["type"] == 'P':
+                data["name"] = "Pikeman"
+                data["type_attack"] = "Melee"
+                data["attack"] = 4
+                data["armor"] = 0
+                data["pierce_armor"] = 0
+                data["range"] = 0.01
+                data["line_of_sight"] = 0
+                data["speed"] = 1
+                data["attack_delay"] = 0
+                data["reload_time"] = 3
+                data["accuracy"] = 1
+            data["property"] = False
+            uid = data["uid"]
+            if uid not in battlefield.troupes:
+                if data ["hp"] != 0:
+                    battlefield.troupes[uid] = Unit(
+                        data["uid"],
+                        data["name"],
+                        data["type"],
+                        data["hp"],
+                        data["type_attack"],
+                        data["attack"],
+                        data["armor"],
+                        data["pierce_armor"],
+                        data["range"],
+                        data["line_of_sight"],
+                        data["speed"],
+                        data["attack_delay"],
+                        data["reload_time"],
+                        data["accuracy"],
+                        (data["x"], data["y"]),
+                        data["property"]
+                    )
+            else:
+                if data["hp"] < battlefield.troupes[uid].hp:
+                    battlefield.troupes[uid].hp = data["hp"]
+                if battlefield.troupes[uid].line_of_sight == 0:
+                    if (battlefield.troupes[uid].position[0] != data["x"]
+                            or battlefield.troupes[uid].position[1] != data["y"]):
+                        battlefield.troupes[uid].position = (data["x"], data["y"])
+                if battlefield.troupes[uid].hp <= 0:
+                    battlefield.remove_unit(uid)
