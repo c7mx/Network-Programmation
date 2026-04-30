cd0b27fe1f09479084e2a8378530fce82cf93578
c7mx <maxime.catteau@insa-cvl.fr>
2026-04-30 16:46:01 +1245

    Refactor update function and adjust unit attributes

--- src/Update.py
+++ src/Update.py
@@ -1,8 +1,6 @@
-import json
 import sys
 import os
 
-# Chemin absolu vers le dossier src, peu importe depuis où on lance le script
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 if SRC_DIR not in sys.path:
     sys.path.insert(0, SRC_DIR)
@@ -10,7 +8,8 @@ if SRC_DIR not in sys.path:
 from model.Unit import Unit
 from model.Battlefield import Battlefield
 
-def update(data_list, battlefield:Battlefield):
+
+def update(data_list, battlefield: Battlefield):
     while data_list:
         data = data_list.pop(0)
 
@@ -21,9 +20,7 @@ def update(data_list, battlefield:Battlefield):
             data["armor"] = 2
             data["pierce_armor"] = 2
             data["range"] = 0.01
-            data["line_of_sight"] = 4
             data["speed"] = 1.35
-            data["attack_delay"] = 0
             data["reload_time"] = 1.8
             data["accuracy"] = 1
 
@@ -34,9 +31,7 @@ def update(data_list, battlefield:Battlefield):
             data["armor"] = 0
             data["pierce_armor"] = 0
             data["range"] = 5
-            data["line_of_sight"] = 7
             data["speed"] = 0.96
-            data["attack_delay"] = 0
             data["reload_time"] = 5
             data["accuracy"] = 0.85
 
@@ -47,34 +42,37 @@ def update(data_list, battlefield:Battlefield):
             data["armor"] = 0
             data["pierce_armor"] = 0
             data["range"] = 0.01
-            data["line_of_sight"] = 4
             data["speed"] = 1
-            data["attack_delay"] = 0
             data["reload_time"] = 3
             data["accuracy"] = 1
 
+        data["line_of_sight"] = 0
+        data["attack_delay"] = 0
         uid = data["uid"]
 
         if uid not in battlefield.troupes:
-            battlefield.troupes[uid] = Unit(
-                data["uid"],
-                data["name"],
-                data["type"],
-                data["hp"],
-                data["type_attack"],
-                data["attack"],
-                data["armor"],
-                data["pierce_armor"],
-                data["range"],
-                data["line_of_sight"],
-                data["speed"],
-                data["attack_delay"],
-                data["reload_time"],
-                data["accuracy"],
-                (data["x"], data["y"])
-            )
+            if data ["hp"] != 0:
+                battlefield.troupes[uid] = Unit(
+                    data["uid"],
+                    data["name"],
+                    data["type"],
+                    data["hp"],
+                    data["type_attack"],
+                    data["attack"],
+                    data["armor"],
+                    data["pierce_armor"],
+                    data["range"],
+                    data["line_of_sight"],
+                    data["speed"],
+                    data["attack_delay"],
+                    data["reload_time"],
+                    data["accuracy"],
+                    (data["x"], data["y"])
+                )
+
         else:
             battlefield.troupes[uid].hp = data["hp"]
             battlefield.troupes[uid].position = (data["x"], data["y"])
+
             if battlefield.troupes[uid].hp <= 0:
                 battlefield.remove_unit(uid)
