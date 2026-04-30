e4f379b81cae5ddade618958fb6a32b19c72d2f1
c7mx <maxime.catteau@insa-cvl.fr>
2026-04-30 17:16:55 +1245

    Refactor command handling and update variable names

--- src/Main.py
+++ src/Main.py
@@ -2,15 +2,11 @@ from Constant import ROWS, COLS, VIEW_ELEVATION
 from util.ScenarioMaker import ScenarioMaker
 from util.ScenarioMaker4 import ScenarioMaker4
 from model.Battlefield import Battlefield
-from model.Battle import Battle
 from model.Battle4 import Battle4
 from model.BattleMulti import BattleMulti
 from view.GUI import GUI
-import network.comm_py_c as NetPy
 from view.Console import Console
 from util.Functions import get_scenario, create_parser, generate_heightmap
-
-
 
 
 def create_view(args, battlefield, generals):
@@ -37,45 +33,12 @@ def setup_environment(args, generals, all_units):
     return battlefield, view
 
 
-
-
 if __name__ == '__main__':
 
     parser = create_parser()
     args = parser.parse_args()
 
-
-    if args.command == 'run':
-        print(f"Running battle between {args.AI1} and {args.AI2}")
-
-        scenario_maker = ScenarioMaker(
-            get_scenario(),
-            args.AI1,
-            args.AI2,
-            id_joueur="0"
-        )
-        data = scenario_maker.get_data()
-
-        generals, all_units = extract_generals(data, 2)
-        battlefield, view = setup_environment(args, generals, all_units)
-
-    
-        sock = NetPy.connect_sock_send()
-        for unit in all_units.values():
-            NetPy.send_data(
-                sock,
-                unit.id,
-                unit.hp,
-                unit.position[0],
-                unit.position[1],
-                unit.symbol
-            )
-
-        battle = Battle(*generals, battlefield, view)
-    
-
-   
-    elif args.command == 'run4':
+    if args.command == 'run4':
         print(f"Running battle between {args.AI1}, {args.AI2}, {args.AI3}, {args.AI4}")
 
         scenario_maker = ScenarioMaker4(
@@ -84,7 +47,7 @@ if __name__ == '__main__':
             args.AI2,
             args.AI3,
             args.AI4,
-            id_joueur="0"
+            player_id="0"
         )
         data = scenario_maker.get_data()
 
@@ -92,21 +55,20 @@ if __name__ == '__main__':
         battlefield, view = setup_environment(args, generals, all_units)
 
         battle = Battle4(*generals, battlefield, view)
-  
-
     
     elif args.command == 'multi':
-        print(f"Bataille avec l'IA {args.AI1}, Joueur ID: {args.id_joueur}")
+        print(f"Battle with {args.AI}, Player ID: {args.player_id}")
 
         scenario_maker = ScenarioMaker(
             get_scenario(),
-            args.AI1,
-            id_joueur=args.id_joueur
+            args.AI,
+            player_id=args.player_id
         )
         data = scenario_maker.get_data()
 
         generals, all_units = extract_generals(data, 1)
         battlefield, view = setup_environment(args, generals, all_units)
 
-        battle = BattleMulti(*generals, battlefield, view, args.id_joueur)
+        battle = BattleMulti(*generals, battlefield, view, args.player_id)
+
     battle.start()
